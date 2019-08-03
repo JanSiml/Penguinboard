167534e16c63c06ded05093fff82bcce7bb61e49
Marc <marc.spoerlein@gmail.com>
2019-08-03 18:58:35 +0200

    Weather Widget V0.2

---
+++ weatherbox.js
@@ -0,0 +1,32 @@
+window.addEventListener('load', ()=> {
+  let long;
+  let lat;
+  let temperatureDescription = document.querySelector(".temperature-description");
+  let temperatureDegree = document.querySelector(".temperature-degree");
+  let locationTimezone = document.querySelector(".location-timezone");
+
+
+  if (navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(position =>{
+      //Log in der Browser-Konsole:
+      //console.log(position)
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+
+      const proxy = "https://cors-anywhere.herokuapp.com/";
+      const api = `${proxy}https://api.darksky.net/forecast/a49243e16acd2b8365860db572961395/${lat},${long}`;
+
+      fetch(api)
+        .then(response =>{
+          return response.json();
+        })
+        .then(data =>{
+          //console.log(data);
+          const {temperature, summary}= data.currently;
+          //Set DOM Elements from the API
+          temperatureDegree.textContent = Math.round(((temperature-32)/1.8)*10)/10;
+
+        });
+    });
+  }
+});
